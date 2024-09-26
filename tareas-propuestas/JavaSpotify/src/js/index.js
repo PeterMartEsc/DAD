535d4882e4cc19cb80f67449b1517aea162db432
PeterMartEsc <petermartesc@gmail.com>
2024-09-26 18:43:58 +0100

    Feat: se trabaja en el javaScript

--- tareas-propuestas/JavaSpotify/src/js/index.js
+++ tareas-propuestas/JavaSpotify/src/js/index.js
@@ -5,6 +5,8 @@ Object.keys(songs);
 const map = {};
 
 let aux = 1;
+
 for (var key of Object.keys(songs)){
-    map[`.item-${aux}`] = `${songs[key]}`;
+    map[`.item-${aux}`] = songs[key];
+    aux++;
 }
