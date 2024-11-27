677801d1e4877be6eb13cb65fdf75da399334d25
PeterMartEsc <petermartesc@gmail.com>
2024-11-27 18:57:17 +0000

    feat: Se hace el primer ejercicio de prueba

---
+++ Vue/ejemplos/mi-primera-vueapp/src/components/PostButton.vue
@@ -0,0 +1,20 @@
+<template>
+    <button class="post-button">Click aqui!</button>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+    .post-button {
+        padding: 10px;
+        background-color: blueviolet;
+        color: white;
+        border: 1px;
+    }
+
+    .post-button:hover{
+        background-color: white;
+        color: blueviolet;
+    }
+</style>
