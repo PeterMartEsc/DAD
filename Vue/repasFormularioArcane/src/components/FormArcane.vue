4f8165fd0d052f2cb672c49e9817676f6f6cb4e0
PeterMartEsc <petermartesc@gmail.com>
2024-12-02 19:41:43 +0000

    feat: Se empieza la app de la tarea de formulario arcane

---
+++ Vue/repasFormularioArcane/src/components/FormArcane.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="formulario">
+        <form>
+            <input type="text" name="nombre" id="nombre">
+            <input type="text" name="origen" id="origen">
+            <input type="text" name="habilidad" id="habilidad">
+            <input type="text" name="aliados" id="aliados">
+            <input type="radio" name="mision-si" id="mision-no">
+        </form>
+    </div>
+</template>
+
+<script>
+    import {defineComponent} from 'vue';
+    import Personaje from '../models/Personaje.js';
+    export default defineComponent({
+        name: 'FromArcane',
+
+        data(){
+            const personajeNuevo = new Personaje();
+            return {
+                personajeNuevo
+            }
+        },
+
+        methods: {
+            saveData(){
+                
+            }
+        }
+    })
+</script>
+
+<style>
+
+</style>
---
+++ Vue/repasFormularioArcane/src/models/Personaje.js
@@ -0,0 +1,14 @@
+class Personaje {
+
+    constructor(nombre, origen,habilidades,aliados,mision) {
+        this.nombre = nombre;
+        this.origen = origen;
+        this.habilidades = habilidades;
+        this.aliados = aliados;
+        this.mision = mision;
+    }
+
+    showInfo(){
+        
+    }
+}
